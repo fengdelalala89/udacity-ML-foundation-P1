a1e10429529e46731a947edd88995f7a4326f216
cao-xh <cao-xh@neusoft.com>
2018-01-31 10:52:01 +0800

    [+]设置utf-8编码；恢复之前的注释

--- Task0.py
+++ Task0.py
@@ -1,3 +1,8 @@
+# -*- coding:utf-8 -*-
+"""
+下面的文件将会从csv文件中读取读取短信与电话记录，
+你将在以后的课程中了解更多有关读取文件的知识。
+"""
 import csv
 
 with open('texts.csv', 'r') as f:
@@ -9,6 +14,9 @@ with open('calls.csv', 'r') as f:
     calls = list(reader)
 
 """
+任务0:
+短信记录的第一条记录是什么？通话记录最后一条记录是什么？
+输出信息:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
